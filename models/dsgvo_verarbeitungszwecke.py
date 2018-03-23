97b32d3473e141619ef2157f8f30a9eb548adc00
marcobewoci <37686641+marcobewoci@users.noreply.github.com>
2018-03-23 09:30:59 +0100

    Delete dsgvo_verarbeitungszwecke.py

--- models/dsgvo_verarbeitungszwecke.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import api, fields, models, osv
-
-class verarbeitungszwecke(models.Model):
-    _name = 'dsgvo.verarbeitungszwecke'
-    name = fields.Char('Verarbeitungszwecke')
-    
