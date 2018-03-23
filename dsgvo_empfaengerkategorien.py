8fa8e9a5615361b9d13ec30ad8c2b2016fa854c8
marcobewoci <37686641+marcobewoci@users.noreply.github.com>
2018-03-23 09:05:12 +0100

    Delete dsgvo_empfaengerkategorien.py

--- dsgvo_empfaengerkategorien.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import api, fields, models, osv
-
-class empfaengerkategorien(models.Model):
-    _name = 'dsgvo.empfaengerkategorien'
-    name = fields.Char('Empfängerkategorien')
-    
