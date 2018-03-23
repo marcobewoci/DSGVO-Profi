96067de634a73431db1e78b9566bde64c0ce851c
marcobewoci <37686641+marcobewoci@users.noreply.github.com>
2018-03-23 09:09:09 +0100

    Add files via upload

---
+++ models/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from . import dsgvo_model
+from . import dsgvo_ver
+from . import dsgvo_vers
+from . import dsgvo_personengruppen
+from . import dsgvo_empfaengerkreis
+from . import dsgvo_empfaengerkategorien
+from . import dsgvo_verarbeitungstaetigkeiten
+from . import dsgvo_verarbeitungszwecke
