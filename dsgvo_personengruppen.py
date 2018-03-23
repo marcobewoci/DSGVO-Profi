cbbb45255ae5e00ee2247d7f587a34c18fa8abdc
marcobewoci <37686641+marcobewoci@users.noreply.github.com>
2018-03-23 09:02:45 +0100

    Delete dsgvo_personengruppen.py

--- dsgvo_personengruppen.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import api, fields, models, osv
-
-class personengruppen(models.Model):
-    _name = 'dsgvo.personengruppen'
-    name = fields.Char('Personengruppen')
-    
