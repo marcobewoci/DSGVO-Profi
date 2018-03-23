3e5b37271091ab22e8f71a485acef4b3c0c4e7b3
marcobewoci <37686641+marcobewoci@users.noreply.github.com>
2018-03-23 09:02:03 +0100

    Delete dsgvo_verarbeitungstaetigkeiten.py

--- dsgvo_verarbeitungstaetigkeiten.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import api, fields, models, osv
-
-class verarbeitungstaetigkeiten(models.Model):
-    _name = 'dsgvo.verarbeitungstaetigkeiten'
-    name = fields.Char('Verarbeitungstätigkeiten')
-    
